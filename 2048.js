6a38cf775f64d6b5479a1ed6ac1a858849f99b16
Vennictus <ishjaap.singh07@gmail.com>
2025-08-20 20:14:12 +0530

    Initial Setup and Basic Styling

---
+++ 2048.js
@@ -0,0 +1,18 @@
+const board = [];
+let score = 0;
+const rows = 4;
+const columns = 4;
+
+window.onload = function(){
+    setGame();
+}
+
+function setGame(){
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    
+}
